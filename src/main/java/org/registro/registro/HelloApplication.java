4632baed3c3aed2e86c93bc1383f24a295644a0b
Manuton04 <manutorres004@gmail.com>
2025-12-22 11:36:15 -0300

    File creation.

--- src/main/java/org/registro/registro/HelloApplication.java
+++ src/main/java/org/registro/registro/HelloApplication.java
@@ -4,20 +4,33 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.registro.registro.classes.Sistema;
 
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+        main();
     }
 
-    public static void main(String[] args) {
-        launch();
+    public static void main() {
+        //Donde esta documentos
+        Path path = Paths.get(
+                System.getProperty("user.home"),
+                "OneDrive",
+                "Documentos",
+                "RegistroMedicoApp"
+        );
+        Sistema sistema = new Sistema(path);
+        try {
+            sistema.inicializarCarpeta();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        //launch();
     }
 }
--- src/main/java/org/registro/registro/classes/Sistema.java
+++ src/main/java/org/registro/registro/classes/Sistema.java
@@ -1,17 +1,22 @@
 package org.registro.registro.classes;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
 public class Sistema {
-    Map<UUID, Paciente> pacientes;
-    Map<UUID, Turno> turnos;
+    private Map<UUID, Paciente> pacientes;
+    private Map<UUID, Turno> turnos;
+    private Path path;
 
-    public Sistema(){
+    public Sistema(Path path){
         pacientes = new HashMap<>();
         turnos = new HashMap<>();
+        this.path = path;
     }
 
     public void addPaciente(Paciente paciente){
@@ -32,6 +37,27 @@ public class Sistema {
 
     public void saveToJson(){
         File folder = new File("data/pacientes");
+    }
+
+    public void savePaciente(Paciente paciente){
+
+    }
+
+    public void inicializarCarpeta() throws IOException {
+        System.out.println("Base path: " + path.toAbsolutePath());
+        System.out.println("Exists? " + Files.exists(path));
+
+        Path pacientesPath = path.resolve("pacientes");
+        System.out.println("Pacientes path: " + pacientesPath.toAbsolutePath());
+
+
+        
+        if (!Files.exists(pacientesPath)) {
+            Files.createDirectories(pacientesPath);
+            System.out.println("Created file pacientes!");
+        } else {
+            System.out.println("Pacientes already existed");
+        }
     }
 
 
